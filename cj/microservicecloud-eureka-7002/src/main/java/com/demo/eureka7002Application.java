fccadc2d167629dd334f0a34bc22585f194a18f5
1024to0215 <1161721536@qq.com>
2019-12-03 10:46:44 +0800

    【feat】 eureka 集群功能实现

---
+++ cj/microservicecloud-eureka-7002/src/main/java/com/demo/eureka7002Application.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class eureka7002Application
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(eureka7002Application.class, args);
+    }
+}
+

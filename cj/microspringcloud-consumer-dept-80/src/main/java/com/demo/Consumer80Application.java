fc67b3ba46ef0fa7f70565033a25590f33a595cc
1024to0215 <1161721536@qq.com>
2019-12-04 14:08:43 +0800

    【fix】 修复客户端负载均衡的问题

--- cj/microspringcloud-consumer-dept-80/src/main/java/com/demo/Consumer80Application.java
+++ cj/microspringcloud-consumer-dept-80/src/main/java/com/demo/Consumer80Application.java
@@ -3,10 +3,12 @@ package com.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class Consumer80Application
 {
     public static void main(String[] args)
--- cj/microspringcloud-consumer-dept-80/src/main/java/com/demo/controller/DeptControllerConsumer.java
+++ cj/microspringcloud-consumer-dept-80/src/main/java/com/demo/controller/DeptControllerConsumer.java
@@ -2,6 +2,7 @@ package com.demo.controller;
 
 import com.demo.entity.Dept;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
